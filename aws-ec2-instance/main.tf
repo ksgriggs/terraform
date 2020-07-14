c2296ce892970f4ba5117661e88cfbe542be0348
Scott Griggs <sgriggs@gmail.com>
2020-07-14 17:03:58 -0400

    Added AWS AMI search

--- aws-ec2-instance/main.tf
+++ aws-ec2-instance/main.tf
@@ -3,6 +3,19 @@ provider "aws" {
     region  = var.aws_region
 }
 
+data "aws_ami" "search" {
+    most_recent = true
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+     }
+ 
+    # Use Amazon Linux 2 AMI (HVM) SSD Volume Type
+    name_regex = "^amzn2-ami-hvm-.*x86_64-gp2"
+    owners = ["137112412989"]
+}
+
 resource "aws_instance" "server" {
     ami             = lookup(var.aws_amis, var.aws_region)
     instance_type   = var.aws_instance_type
