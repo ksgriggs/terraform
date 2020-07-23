cb432f983ee840773896acce7026946931dbebd2
Scott Griggs <sgriggs@gmail.com>
2020-07-23 14:22:28 -0400

    Moved variables to variables.tf

--- aws-ec2-instance/variables.tf
+++ aws-ec2-instance/variables.tf
@@ -1,1 +1,9 @@
-#TODO Scott move your variables here.
+variable "instance_type" {
+  description = "AWS instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of AWS key pair."
+  type        = string
+}
