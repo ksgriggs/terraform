5b42e3ad83197bf494401b2597acc51d5109414f
Scott Griggs <sgriggs@gmail.com>
2020-08-13 00:59:15 -0400

    Resource name changes

--- aws-vpc/main.tf
+++ aws-vpc/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region  = var.aws_region
 }
 
-resource "aws_vpc" "tf_vpc" {
+resource "aws_vpc" "default" {
   cidr_block           = var.vpc_CIDR_block
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -14,8 +14,8 @@ resource "aws_vpc" "tf_vpc" {
   }
 }
 
-resource "aws_default_network_acl" "tf_nacl" {
-  default_network_acl_id = aws_vpc.tf_vpc.default_network_acl_id
+resource "aws_default_network_acl" "default" {
+  default_network_acl_id = aws_vpc.default.default_network_acl_id
 
   ingress {
     protocol   = -1
@@ -40,24 +40,24 @@ resource "aws_default_network_acl" "tf_nacl" {
   }
 }
 
-resource "aws_internet_gateway" "tf_igw" {
-  vpc_id = aws_vpc.tf_vpc.id
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
 
   tags = {
     "Name" = "tf_igw"
   }
 }
 
-resource "aws_default_route_table" "tf_route_table" {
-  default_route_table_id = aws_vpc.tf_vpc.default_route_table_id
+resource "aws_default_route_table" "default" {
+  default_route_table_id = aws_vpc.default.default_route_table_id
 
   tags = {
     "Name" = "tf_route_table"
   }
 }
 
-resource "aws_subnet" "tf_subnet_public_az1" {
-  vpc_id                  = aws_vpc.tf_vpc.id
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.default.id
   cidr_block              = var.subnet_CIDR_block
   availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
@@ -67,19 +67,19 @@ resource "aws_subnet" "tf_subnet_public_az1" {
   }
 }
 
-resource "aws_route_table_association" "tf_subnet_public_az1" {
-  subnet_id      = aws_subnet.tf_subnet_public_az1.id
-  route_table_id = aws_vpc.tf_vpc.default_route_table_id
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_vpc.default.default_route_table_id
 }
 
-resource "aws_route" "tf_igw_public" {
-  route_table_id = aws_default_route_table.tf_route_table.id
+resource "aws_route" "public_internet_gateway" {
+  route_table_id = aws_default_route_table.default.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.tf_igw.id
+  gateway_id = aws_internet_gateway.default.id
 }
 
-resource "aws_default_security_group" "tf_sg_public" {
-  vpc_id = aws_vpc.tf_vpc.id
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.default.id
 
   # Allow ingress on port 22
   ingress {
