f579d6af7b8267469df1f6d1a11ab2ca9d028e56
Scott Griggs <sgriggs@gmail.com>
2020-08-13 01:10:30 -0400

    Initial commit

--- aws-vpc/output.tf
+++ aws-vpc/output.tf
@@ -1,1 +1,17 @@
 # output.tf
+
+output public_subnet_id {
+    value = aws_subnet.public.id
+}
+
+output vpc {
+    value = aws_vpc.default
+}
+
+output network_info {
+    value = {
+        vpc_id     = aws_vpc.default.id
+        subnet_id  = aws_subnet.public.id
+        cidr_block = aws_vpc.default.cidr_block
+    }
+}
